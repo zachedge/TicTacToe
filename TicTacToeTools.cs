b88a238489cb2e422cd1fa3a39e5130f1fbd32ff
youneedanemail <youneedanemail@gmail.com>
2024-02-01 16:13:36 -0700

    Initial attempt with both methods

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -8,5 +8,64 @@ namespace TicTacToe
 {
     internal class TicTacToeTools
     {
+        public string PrintBoard(boardArray)
+        {
+            string result = "";
+            
+            for(int i=0; i < 3; i++)
+            {
+                for(int y=0;  < 3; i++)
+                {
+                    char mark = "";
+
+                    if (boardArray[i][y] == 0)
+                    {
+                        mark = " ";
+                    }
+                    else if (boardArray[i][y] == 1)
+                    {
+                        mark = "X";
+                    }
+                    else if (boardArray[i][y] == 2)
+                    {
+                        mark = "O";
+                    }
+
+                    Console.Write(mark + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+        pubic int evalutateWin(int[,] boardArray)
+        {
+            // Check rows, columns, and diagonals for a winner
+            for (int i = 0; i < 3; i++)
+            {
+                string result = " ";
+
+                if (board[i, 0] == ‘1’ && board[i, 1] == ‘1’ && board[i, 2] == ‘1’)
+                    result = "X wins"; // Player X wins in a row
+                if (board[i, 0] == ‘2’ && board[i, 1] == ‘2’ && board[i, 2] == ‘2’)
+                    result = "O wins"; // Player O wins in a row
+                if (board[0, i] == ‘1’ && board[1, i] == ‘1’ && board[2, i] == ‘1’)
+                    result = "X wins"; // Player X wins in a column
+                if (board[0, i] == ‘2’ && board[1, i] == ‘2’ && board[2, i] == ‘2’)
+                    result = "O wins"; // Player O wins in a column
+            }
+            if (board[0, 0] == ‘1’ && board[1, 1] == ‘1’ && board[2, 2] == ‘1’)
+                result = "X wins"; // Player X wins diagonally
+            if (board[0, 0] == ‘2’ && board[1, 1] == ‘2’ && board[2, 2] == ‘2’)
+                result = "O wins"; // Player O wins diagonally
+            if (board[0, 2] == ‘1’ && board[1, 1] == ‘1’ && board[2, 0] == ‘1’)
+                result = "X wins"; // Player X wins diagonally
+            if (board[0, 2] == ‘2’ && board[1, 1] == ‘2’ && board[2, 0] == ‘2’)
+                result = "O wins"; // Player O wins diagonally
+            return result;
+        }
+}
     }
 }
